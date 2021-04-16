4cd63757581bfb928e915275c218a5a80fc108c4
Pankaj Karwa <Pankaj_Karwa@epam.com>
2021-04-16 21:55:59 +0530

    initial commit

---
+++ guest-service/src/main/java/com/epam/guestservice/exception/ExceptionResponce.java
@@ -0,0 +1,39 @@
+package com.epam.guestservice.exception;
+
+import java.util.Date;
+
+public class ExceptionResponce {
+	
+	private String message;
+	private String details;
+	private Date timestamp;
+	
+	public ExceptionResponce(String message, String details, Date timestamp) {
+		super();
+		this.message = message;
+		this.details = details;
+		this.timestamp = timestamp;
+	}
+	
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}

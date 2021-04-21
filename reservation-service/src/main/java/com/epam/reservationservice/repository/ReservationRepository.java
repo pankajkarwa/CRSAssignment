0f1d66320c634510f3a28c96bc874b3226d48f82
Pankaj_Karwa <Pankaj_Karwa@EPINPUNW00D9.princeton.epam.com>
2021-04-21 14:39:50 +0530

    Added JPA dependency for Reservation API

--- reservation-service/src/main/java/com/epam/reservationservice/repository/ReservationRepository.java
+++ reservation-service/src/main/java/com/epam/reservationservice/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.epam.reservationservice.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.epam.reservationservice.model.Reservation;
 
 
 @Repository
-public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+public interface ReservationRepository extends  CrudRepository<Reservation, Long> {
 
 }
